3d7e7f1bec4d8cdfe904cff3a12c3b38d0a3e233
jackfok <jackfok905@gmail.com>
2017-03-24 19:00:52 +0800

    Add files via upload

---
+++ en.inc.php
@@ -0,0 +1,16 @@
+<?php
+$organ = "Hong Kong Amateur Athletic Association";
+$event = "Standard Chartered Hong Kong Marathon 2017";
+
+$appform = "Runner Application Form";
+$fname = "First Name :";
+$lname = "Last Name :";
+$sex = "Sex :";
+$hkid = "HKID :";
+$email = "Email Address :";
+$connum = "Contact Number :";
+$addr = "Address :";
+
+$sddetail = "Email me more details of the Marathon";
+
+$cpright = "All right reserved";
